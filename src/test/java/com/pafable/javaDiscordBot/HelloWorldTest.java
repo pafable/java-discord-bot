b0fecebb24d6761059905aac57afbc558c50cbd5
pafable <philafable@gmail.com>
2023-04-13 22:48:37 -0400

    added test

---
+++ src/test/java/com/pafable/javaDiscordBot/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.pafable.javaDiscordBot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+
+    @Test
+    void helloWorld() {
+        System.out.println("Hello world!");
+    }
+}
